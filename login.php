f44bfcb0c2c5ecad23f56268c1ddd46539e57b78
Vala Zeinali <32586254+vala9999@users.noreply.github.com>
2019-04-23 18:08:41 -0400

    Add files via upload
    Login Page code from displaypage

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+   <body>
+   <div style="height:900px; background-color: orange;" align="center">  
+	  <br><br><br><br>
+      <p>Enter Instructor information for insertion <br> </p>
+      <form method = "post" action = "<?php $_PHP_SELF ?>">
+         <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
+            <tr>
+               <td width = "250">Email</td>
+               <td>
+                  <input name = "email" type = "text" id = "email">
+               </td>
+            </tr>
+         
+            <tr>
+               <td width = "250">Password</td>
+               <td>
+                  <input name = "pass" type = "text" id = "pass">
+               </td>
+            </tr>
+         
+            <tr>
+               <td width = "250"> </td>
+               <td>
+                  <input name = "add" type = "submit" id = "add"  value = "Submit">
+               </td>
+            </tr>
+			
+         </table>
+		 <table>
+		 <br><br>
+         <tr><td><image src="flight.jpg" width="275" height="150"></td></tr>
+         </table>
+   
+	<br><br>
+   <hr width="50">
+<a href="Frontpage.html" style="color:black;font-weight:bold;">Home</a>
+<hr width="50">
+   </div>
+   
+   </body>
+</html>
