30f7973364aafd060e6a725e9bbdce9b1293e149
mustachematt <49923927+mustachematt@users.noreply.github.com>
2019-04-26 12:52:30 -0400

    Create airport_db_reset.sql
    I figured we could use this in case we need a way of resetting the contents of the database.

---
+++ airport_db_reset.sql
@@ -0,0 +1,50 @@
+drop table flight;
+drop table plane;
+drop table ticket_holder;
+drop table ticket_for_flight;
+drop table plane_makes_flight;
+
+create table flight (
+	flight_id varchar(8),
+	flight_depart_timestamp timestamp not null,
+	ETA timestamp,
+	destination_airport varchar(64) not null,
+	departure_airport varchar(64) not null,
+	primary key (flight_id)
+	);
+	
+create table plane (
+	plane_id varchar(8),
+	first_class_seats numeric(3,0) check (first_class_seats >= 0),
+	second_class_seats numeric(3,0) check (second_class_seats >= 0),
+	economy_seats numeric(3,0) check (economy_seats >= 0),
+	primary key (plane_id)
+	);
+	
+create table ticket_holder (
+	ticket_id varchar(16),
+	seating_type varchar(32) not null,
+	price numeric(5,2) check (price > 0),
+	ticket_depart_timestamp timestamp not null,
+	username varchar(32),
+	primary key (ticket_id)
+	);
+	
+create table ticket_for_flight (
+	ticket_id varchar(16),
+	flight_id varchar(8),
+	primary key (ticket_id, flight_id),
+	foreign key (ticket_id) references ticket_holder (ticket_id),
+	foreign key (flight_id) references flight (flight_id)
+	on delete cascade
+	);
+	
+create table plane_makes_flight (
+	flight_id varchar(8),
+	plane_id varchar(8),
+	primary key (flight_id, plane_id),
+	foreign key (flight_id) references flight (flight_id),
+	foreign key (plane_id) references plane (plane_id)
+	on delete cascade
+	);	
+	
