22f8b6b14c2f5a2d379cabd0781b909934124258
mustachematt <49923927+mustachematt@users.noreply.github.com>
2019-04-30 19:18:07 -0400

    Add files via upload
    sorry, these should be the most up-to-date files

--- airport_tables.sql
+++ airport_tables.sql
@@ -16,29 +16,17 @@ create table plane (
 	);
 	
 create table ticket_holder (
-	ticket_id varchar(16),
+	ticket_id varchar(8),
+	flight_id varchar(8),
+	plane_id varchar(8),
 	seating_type varchar(32) not null,
 	price numeric(5,2) check (price > 0),
 	ticket_depart_timestamp timestamp not null,
 	username varchar(32),
-	primary key (ticket_id)
+	primary key (ticket_id),
+	foreign key (flight_id) references flight(flight_id),
+	foreign key (plane_id) references plane(plane_id)
 	);
 	
-create table ticket_for_flight (
-	ticket_id varchar(16),
-	flight_id varchar(8),
-	primary key (ticket_id, flight_id),
-	foreign key (ticket_id) references ticket_holder (ticket_id),
-	foreign key (flight_id) references flight (flight_id)
-	on delete cascade
-	);
-	
-create table plane_makes_flight (
-	flight_id varchar(8),
-	plane_id varchar(8),
-	primary key (flight_id, plane_id),
-	foreign key (flight_id) references flight (flight_id),
-	foreign key (plane_id) references plane (plane_id)
-	on delete cascade
-	);	
+
 	
